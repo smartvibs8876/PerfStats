4fa04a7bb82fd1871fbd62df8a004cbc8ac1dbe9
smartvibs8876 <vibhavdhaimode@gmail.com>
2022-06-22 14:05:06 +0530

    Added workflow template

--- Comparison.py
+++ Comparison.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import sys
 
 class Comparison:
     def __init__(self,file1,file2):
@@ -13,11 +14,25 @@ class Comparison:
         file2.close()
     
     def compare(self):
+        try:
+            package1,release1 = sys.argv[1].split("-")
+            release1 = release1.replace(".json","")
+            package2, release2 = sys.argv[2].split("-")
+            release2 = release2.replace(".json","")
+        except IndexError:
+            print("Invalid command line arguments")
+        
         if float(self.file1Contents["seconds time elapsed"]) < float(self.file2Contents["seconds time elapsed"]):
-            self.sendSlackNotification(self.file1+" is better")
+            difference = float(self.file2Contents["seconds time elapsed"]) - float(self.file1Contents["seconds time elapsed"])
+            message = "package " + package1 + " of release " + release1 + " is better than " + "package " + package2 + " of release " + release2 + " by "+ str(difference)
+            print(message)
+            #self.sendSlackNotification(message)
         else:
-            self.sendSlackNotification(self.file2+" is better")
-    
+            difference = float(self.file1Contents["seconds time elapsed"]) - float(self.file2Contents["seconds time elapsed"])
+            message = "package " + package2 + " of release " + release2 + " is better than " + "package "+ package1 + " of release " + release1 + " by "+ str(difference)
+            print(message)
+            #self.sendSlackNotification(message)
+
     def sendSlackNotification(self,message):
         #curl -X POST -H 'Content-type: application/json' --data '{"text":"'${nodeStatusDictionary[$nodeName]}'"}' https://hooks.slack.com/services/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
         url = 'https://hooks.slack.com/services/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'
@@ -25,5 +40,8 @@ class Comparison:
         request = requests.post(url, json = payload)
         print(request.text)
 
-comparison = Comparison("output1.json","output2.json")
-comparison.compare()
+try:
+    comparison = Comparison(sys.argv[1],sys.argv[2])
+    comparison.compare()
+except IndexError:
+    print("Invalic command line arguments")
